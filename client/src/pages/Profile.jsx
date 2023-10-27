46412ad6ee363a509474f43a917bf323b78e7106
careenNinja Admin <webadmin@careerninja.in>
2023-10-27 20:24:49 +0530

    Added update page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
-import { useSelector } from "react-redux"
-import { selectUser } from "../redux/user/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { selectUser, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice"
 import { useEffect, useRef, useState } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from "../../firebase"
@@ -7,21 +7,23 @@ import { app } from "../../firebase"
 
 const Profile = () => {
 
-    const { currentUser } = useSelector(selectUser)
+    const { currentUser, loading, error } = useSelector(selectUser)
     const fileRef = useRef(null)
     const [ file, setFile ] = useState(undefined)
     const [ fileProgress, setFileProgress ] = useState(0)
     const [ fileError, setFileError ] = useState(false)
     const [ errorMessage, setErrorMessage ] = useState('')
     const [ formData, setFormData ] = useState({})
+    const [ updateSuccess, setUpdateSuccess ] = useState(false)
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
         if (file){
             handleFileUpload(file)
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[file])
-
-    console.log(fileProgress)
 
     const handleFileUpload = (file) => {
 
@@ -47,12 +49,40 @@ const Profile = () => {
         })
     }
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value })
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            dispatch(updateUserStart())
+            const res = await fetch(`/api/user/update/${currentUser._id}`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            const data = await res.json()
+            if (data.success === false) {
+                dispatch(updateUserFailure(data.message))
+                return
+            }
+            dispatch(updateUserSuccess(data))
+            setUpdateSuccess(true)
+        } catch (error) {
+            dispatch(updateUserFailure(error.message))
+        }
+    }
+
     console.log(errorMessage)
+    console.log(formData)
 
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-            <form className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                 <input type="file" onChange={(e) => setFile(e.target.files[0])} name="" id="" ref={fileRef} hidden accept="image/*"/>
                 <img onClick={() => fileRef.current.click()} src={formData?.avatar ? formData?.avatar : currentUser?.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
                 <p className="text-center text-sm">
@@ -61,27 +91,34 @@ const Profile = () => {
                 <input 
                     type="text" 
                     placeholder="Username"
+                    defaultValue={currentUser.username}
                     className="border p-3 rounded-lg"
                     id="username"
+                    onChange={handleChange}
                 />
                 <input 
                     type="password" 
                     placeholder="Password"
                     className="border p-3 rounded-lg"
                     id="password"
+                    onChange={handleChange}
                 />
                 <input 
                     type="email" 
                     placeholder="Email"
+                    defaultValue={currentUser.email}
                     className="border p-3 rounded-lg"
                     id="email"
+                    onChange={handleChange}
                 />
-                <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95">Update</button>
+                <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95">{loading ? "Loading..." : "Update" }</button>
             </form>
             <div className="flex justify-between mt-5">
                 <span className="text-red-700 cursor-pointer">Delete Account</span>
                 <span className="text-red-700 cursor-pointer">Sign Out</span>
             </div>
+            <p className="text-red-700">{error ? error : null}</p>
+            <p className="text-green-700 mt-5">{updateSuccess ? "User is updated successfully" : null}</p>
         </div>
     )
 }
