ea4d5fec51417ddb120479fe6103bca021ba4fdd
careenNinja Admin <webadmin@careerninja.in>
2023-11-01 19:11:39 +0530

    Completed landing Page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -11,8 +11,10 @@ import {
     // FaMapMarkedAlt,
     FaMapMarkerAlt,
     FaParking,
-    // FaShare,
+    FaShare,
   } from 'react-icons/fa';
+import { useSelector } from 'react-redux'
+import Contact from '../components/Contact'
 
 const Listing = () => {
 
@@ -20,6 +22,9 @@ const Listing = () => {
     const [ listing, setListing ] = useState(null)
     const [ loading, setLoading ] = useState(true)
     const [ error, setError ] = useState(false)
+    const [copied, setCopied] = useState(false)
+    const [contact, setContact] = useState(false)
+    const { currentUser } = useSelector((state) => state.user);
 
     const { id } =  useParams()
 
@@ -43,9 +48,8 @@ const Listing = () => {
             }
         }
         fetchListing()
-    }, [])
+    }, [id])
 
-    console.log(listing)
     return (
         <main className=''>
             {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
@@ -63,6 +67,18 @@ const Listing = () => {
                         </SwiperSlide>
                     ))}
                 </Swiper>
+                <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+                    <FaShare className='text-slate-500' onClick={() => { 
+                        navigator.clipboard.writeText(window.location.href);
+                        setCopied(true)
+                        setTimeout(() => { setCopied(false)}, 2000)}}
+                    />
+                </div>
+                {copied && (
+                    <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+                    Link copied!
+                    </p>
+                )}
                 <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
                     <p className='text-2xl font-semibold'>
                         {listing.name} - ${' '}
@@ -111,14 +127,14 @@ const Listing = () => {
                             {listing.furnished ? 'Furnished' : 'Unfurnished'}
                         </li>
                     </ul>
-                    {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
+                    {currentUser && listing.userRef !== currentUser._id && !contact && (
                     <button
                         onClick={() => setContact(true)}
                         className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
                     >
                         Contact landlord
                     </button>)}
-                    {contact && <Contact listing={listing} />} */}
+                    {contact && <Contact listing={listing} />}
                 </div>
             </>}
 
