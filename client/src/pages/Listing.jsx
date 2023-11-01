c752c29df2b8d978637cb8c41189bae9e1d82d4b
careenNinja Admin <webadmin@careerninja.in>
2023-11-01 09:01:01 +0530

    completed listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,15 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper'
 import { Navigation } from 'swiper/modules'
 import 'swiper/css/bundle'
+import {
+    FaBath,
+    FaBed,
+    FaChair,
+    // FaMapMarkedAlt,
+    FaMapMarkerAlt,
+    FaParking,
+    // FaShare,
+  } from 'react-icons/fa';
 
 const Listing = () => {
 
@@ -38,7 +47,7 @@ const Listing = () => {
 
     console.log(listing)
     return (
-        <main>
+        <main className=''>
             {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
             {error && <p className='text-center my-7 text-2xl text-red-700'>Something Went Wrong</p>}
             {listing && !error && !loading && <>
@@ -54,6 +63,63 @@ const Listing = () => {
                         </SwiperSlide>
                     ))}
                 </Swiper>
+                <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+                    <p className='text-2xl font-semibold'>
+                        {listing.name} - ${' '}
+                        {listing.offer
+                            ? listing.discountPrice.toLocaleString('en-US')
+                            : listing.regularPrice.toLocaleString('en-US')}
+                        {listing.type === 'rent' && ' / month'}
+                    </p>
+                    <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+                        <FaMapMarkerAlt className='text-green-700' />
+                        {listing.address}
+                    </p>
+                    <div className='flex gap-4'>
+                        <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                            {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                        </p>
+                        {listing.offer && (
+                            <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                            ${+listing.regularPrice - +listing.discountPrice} OFF
+                            </p>
+                        )}
+                    </div>
+                    <p className='text-slate-800'>
+                    <span className='font-semibold text-black'>Description - </span>
+                    {listing.description}
+                    </p>
+                    <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                        <li className='flex items-center gap-1 whitespace-nowrap '>
+                            <FaBed className='text-lg' />
+                            {listing.bedRooms > 1
+                            ? `${listing.bedRooms} beds `
+                            : `${listing.bedRooms} bed `}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap '>
+                            <FaBath className='text-lg' />
+                            {listing.bathRooms > 1
+                            ? `${listing.bathRooms} baths `
+                            : `${listing.bathRooms} bath `}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap '>
+                            <FaParking className='text-lg' />
+                            {listing.parking ? 'Parking spot' : 'No Parking'}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap '>
+                            <FaChair className='text-lg' />
+                            {listing.furnished ? 'Furnished' : 'Unfurnished'}
+                        </li>
+                    </ul>
+                    {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
+                    <button
+                        onClick={() => setContact(true)}
+                        className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+                    >
+                        Contact landlord
+                    </button>)}
+                    {contact && <Contact listing={listing} />} */}
+                </div>
             </>}
 
         </main>
